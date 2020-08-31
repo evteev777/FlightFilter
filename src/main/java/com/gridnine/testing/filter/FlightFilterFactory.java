7eca12df30d53df21a7c1c1a8725909916067e01
Александр Евтеев <evteev777@gmail.com>
2020-08-31 05:59:28 +0300

    Fix application.yaml

--- src/main/java/com/gridnine/testing/filter/FlightFilterFactory.java
+++ src/main/java/com/gridnine/testing/filter/FlightFilterFactory.java
@@ -2,6 +2,9 @@ package com.gridnine.testing.filter;
 
 import com.gridnine.testing.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,28 +12,40 @@ import java.util.Properties;
 
 public class FlightFilterFactory {
 
-    private static final String PROPERTIES = "application.yaml";
-    private static final String PACKAGE = "com.gridnine.testing.filter_impls.";
     private static final FlightFilterFactory INSTANCE;
+    private static final String PACKAGE = "com.gridnine.testing.filter_impls.";
+    private static final File PROPERTY_FILE = new File("application.yaml");
+    private static final String FLIGHT_FILTERS = "flightFilters";
+    private static final String CHECK_CONNECTION = "check-connection";
 
     private final List<FlightFilter> flightFilters = new ArrayList<>();
 
     private FlightFilterFactory() {
-        Properties props = new Properties();
-        try {
-            props.load(this.getClass().getResourceAsStream(PROPERTIES));
-            String flightFiltersProp = props.getProperty("flightFilters");
+        Properties property = new Properties();
+
+        try (FileInputStream fis = new FileInputStream(PROPERTY_FILE)) {
+
+            property.load(fis);
+
+            Log.info(property.getProperty(CHECK_CONNECTION));
+            Log.info("Get filters from Application.yaml");
+            String flightFiltersProp = property.getProperty(FLIGHT_FILTERS);
 
             if (flightFiltersProp == null || flightFiltersProp.equals("")) {
                 throw new IllegalArgumentException("Filters are not specified, " +
-                        "all flights are shown. Check application.yaml !!!");
+                        "Check Application.yaml !!!]n" +
+                        "Flights are shown without filters:");
             }
 
             for (String flightFilter : flightFiltersProp.split(" ")) {
                 addFlightFilter(flightFilter);
             }
-        } catch (Exception e) {
-            Log.error(e);
+        } catch (IllegalArgumentException e) {
+            Log.error(e.getMessage(), e);
+        } catch (IOException e) {
+            Log.error("Properties file not found! Put Application.yaml " +
+                    "in the same folder as FlightFilter.jar !!!\n" +
+                    "Flights are shown without filters:", e);
         }
     }
 
@@ -46,8 +61,8 @@ public class FlightFilterFactory {
                 NoSuchMethodException | SecurityException |
                 ClassNotFoundException e) {
             Log.error("Filter: " + flightFilter +
-                    " - Invalid filter name, not applied. " +
-                    "Check application.yaml !!!", e);
+                    " - Invalid filter name. Filter not applied. " +
+                    "Check Application.yaml !!!", e);
         }
     }
 
